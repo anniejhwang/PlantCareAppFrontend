65ae47e2b23c471dcb959a36ceeb234265c7db04
Annie <anniehwang428@gmail.com>
2023-05-05 17:30:59 -0400

    fixes axios error

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./index.css";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
 
 axios.defaults.baseURL =
   process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://plantcareapp.onrender.com";
